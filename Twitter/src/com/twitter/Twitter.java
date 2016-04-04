a2a6a8160bdce42d1fa3e3d9b3ac9827072f355d
nevenapesic96 <npnevenap@gmail.com>
2016-04-04 11:27:59 +0200

    Napravljena dokumentacija za klasu Twitter

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -2,31 +2,49 @@ package com.twitter;
 
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * 
+ * @author Nevena Pesic
+ * @version V1.0
+ *
+ */
 public class Twitter {
+	/**
+	 * Lista poruka koje sadrze naziv korisnika i tekst poruke
+	 */
 	private LinkedList<TwitterPoruka> poruke =
 			new LinkedList<TwitterPoruka>();
-	public LinkedList<TwitterPoruka> vratiSvePoruke(){
+	/**
+	 * 
+	 * @return Sve poruke iz liste
+	 */
+public LinkedList<TwitterPoruka> vratiSvePoruke(){
 		return poruke;
 }
+/**
+ * Pravi novu poruku, puni je podacima koji su prosledjeni i tu poruku unosi na kraj liste
+ * @param korisnik Naziv korisnika
+ * @param poruka Tekst poruke
+ */
 public void unesi(String korisnik, String poruka) {
-//Pravi se nova poruka i puni podacima.
 	TwitterPoruka tp = new TwitterPoruka();
 	tp.setKorisnik("korisnik");
 	tp.setPoruka(poruka);
-//Poruka se unosi u listu na kraj
 	poruke.addLast(tp);
 }
+/**
+ * Vraca niz poruka koje u sebi sadrze zadati tag
+ * @param maxBroj Maksimalni broj poruka
+ * @param tag Tag koji se trazi
+ * @return Sve poruke koje u sebi imaju zadati tag
+ * @throws RuntimeException Ako tag nije unet
+ */
 public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 	if (tag==null || tag == "")
 		throw new RuntimeException("Morate uneti tag");
-//Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
 	if (maxBroj<=0)
 		maxBroj = 100;
- //Pomocna promenljiva koja predstavlja brojac upisanih poruka
 	int brojac = 0; 
-//Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
-//sve poruke koje u sebi imaju zadati tag
 	TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
 //Pretrazuju se poruke i traze se one koje sadrze tag.
 //Ako se nadje neka takva, i ako nije prekoracen maxBroj
